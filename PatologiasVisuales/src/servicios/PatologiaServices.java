1ec8d657c8a78d6c716638d5ad5cd1d0f17dbe80
SusanaSanch <susanasanchez@auxiliamadrid.org>
2017-02-23 18:56:24 +0100

    Log en Patologias Visuales Registros log funcionando

--- PatologiasVisuales/src/servicios/PatologiaServices.java
+++ PatologiasVisuales/src/servicios/PatologiaServices.java
@@ -1,11 +1,14 @@
 package servicios;
 
+import org.apache.log4j.Logger;
+
 import dao.PatologiaDAO;
 import dto.MapaPatologias;
 import dto.PatologiaDTO;
 
 public class PatologiaServices {
 
+	private final static Logger log = Logger.getLogger("mylog");
 	
 	public PatologiaDTO buscarPatologiaPorId(int id) throws Throwable
 	{
@@ -16,6 +19,8 @@ public class PatologiaServices {
 		
 		pato_dto = MapaPatologias.getMapa_patologias().get(id);
 		System.out.println(pato_dto.getNombre() + " - PatologiaServices");
+		
+		log.info(pato_dto.getNombre() + " - PatologiaServices");
 		
 		return pato_dto;
 	}
